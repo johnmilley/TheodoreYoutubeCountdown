14df6b0572ee4622b35790b4d231b4ba57df6fe4
johnmilley <mushmushthemarvelousmonkey@hotmail.com>
2023-03-29 20:04:56 -0230

    removed weeks

--- youtube_countdown.js
+++ youtube_countdown.js
@@ -1,5 +1,5 @@
 // march 28, 2028, 12:00pm
-videoReleaseDay = new Date(2028, 2, 28, 12);
+videoReleaseDay = new Date(2028, 2, 28);
 
 // get the time in seconds between now and the big day
 
@@ -10,16 +10,14 @@ function timeremaining(targetDate) {
     let seconds = Math.floor(secondsRemaining % 60)
     let minutes = Math.floor((secondsRemaining / 60) % 60)
     let hours = Math.floor((secondsRemaining / 3600) % 24)
-    let days = Math.floor((secondsRemaining / 86400) % 7)
-    let weeks = Math.floor((secondsRemaining / 604800) % 52)
+    let days = Math.floor((secondsRemaining / 86400) % 365)
     let years = Math.floor((secondsRemaining / 31536000))
 
-    updateDisplay(seconds, minutes, hours, days, weeks, years)
+    updateDisplay(seconds, minutes, hours, days, years)
 }
 
-function updateDisplay(seconds, minutes, hours, days, weeks, years) {
+function updateDisplay(seconds, minutes, hours, days, years) {
     document.querySelector('#years').innerHTML = years.toFixed(0)
-    document.querySelector('#weeks').innerHTML = weeks.toFixed(0)
     document.querySelector('#days').innerHTML = days.toFixed(0)
     document.querySelector('#hours').innerHTML = hours.toFixed(0)
     document.querySelector('#minutes').innerHTML = minutes.toFixed(0)
